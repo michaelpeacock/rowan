399e76c08cec514851cc368da4cde4a9986af84f
Lark-wrc <willowlark@outlook.com>
2015-12-18 13:34:06 -0500

    Project Initalized

---
+++ Hoth Radar/src/com/example/Hoth_Radar/Splash.java
@@ -0,0 +1,17 @@
+package com.example.Hoth_Radar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
