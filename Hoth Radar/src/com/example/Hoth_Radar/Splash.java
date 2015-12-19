90e447430f9a17696ef69b167a6137e36fd0d1c0
Lark-wrc <willowlark@outlook.com>
2015-12-18 21:48:13 -0500

    splash testing

--- Hoth Radar/src/com/example/Hoth_Radar/Splash.java
+++ Hoth Radar/src/com/example/Hoth_Radar/Splash.java
@@ -2,9 +2,18 @@ package com.example.Hoth_Radar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class Splash extends Activity
 {
+
+    private Button buildButton;
+    private Button startButton;
+
     /**
      * Called when the activity is first created.
      */
@@ -13,6 +22,24 @@ public class Splash extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        buildButton = (Button)findViewById(R.id.BuildButton);
+        buildButton.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+            }
+        });
+        startButton = (Button)findViewById(R.id.StartButton);
+        startButton.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
